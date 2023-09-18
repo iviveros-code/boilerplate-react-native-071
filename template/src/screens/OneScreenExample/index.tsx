25b577d9f9bfae4f6c79e89522e84ebc12fb2d03
Ivan Viveros <ivan.d.viveros@gmail.com>
2023-09-18 19:15:39 -0300

    fix vector icons in android

--- template/src/screens/OneScreenExample/index.tsx
+++ template/src/screens/OneScreenExample/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView, Text, Keyboard, View } from 'react-native'
 import { useTranslation } from 'react-i18next'
 
 import { TextInputComponent } from '@components'
@@ -7,12 +7,32 @@ import { TextInputComponent } from '@components'
 import { styles } from './styles'
 
 export const OneScreenExample = () => {
+  const [text, setText] = useState('')
+
+  const cleanInput = () => {
+    setText('')
+  }
+
   const { t } = useTranslation()
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, { paddingHorizontal: 20 }]}>
       <Text>{t('Home.welcome')}</Text>
-      <TextInputComponent label='Email' value='test' placeholder='Enter your email' right={true} />
+
+      <View style={{ flex: 1, width: '100%' }}>
+        <TextInputComponent
+          label={'placeholder'}
+          value={text}
+          onChangeText={setText}
+          right={text}
+          actionX={cleanInput}
+          onSubmitEditing={Keyboard.dismiss}
+          keyboardType='default'
+          mode='outlined'
+          autoFocus={true}
+          inlineImageLeft='search_icon'
+        />
+      </View>
     </SafeAreaView>
   )
 }
